ca01e1b40b8e512c920b6c8aa48eae781cfcffbb
Vinay Krishnamurthy <vinayk.url@gmail.com>
2013-02-09 14:17:06 -0500

    Part B exercise 3 completed; exercise still pending

--- hw1_classes.py
+++ hw1_classes.py
@@ -1,24 +1,21 @@
+
 class PBF:
     # pass
 
     def isNNF(self):
         postfix_str = self.__str__()
+        postfix_str = postfix_str.replace(' ', '')
         for idx in range(len(postfix_str)):
             if(postfix_str[idx] == '!'):
                 # Go back 2 postions(including space), and check
-                if(postfix_str[idx - 2] == '&' or postfix_str[idx - 2] == '|'):
+                if(postfix_str[idx - 1] == '&' or postfix_str[idx - 1] == '|'):
                     return False
         
         return True
 
     def toNNF(self):
-        nnf_str = []
-        
-        return nnf_str
-
-    def parse(postfix_str):
-        return 
-        
+        nnfObj = PBF()
+        return nnfObj        
 
 class OR(PBF):
     
@@ -29,7 +26,6 @@ class OR(PBF):
     def __str__(self):
         return str(self.lchild) + " " + str(self.rchild) + " |"
 
-
 class AND(PBF):
 
     def __init__(self, f1, f2):
@@ -39,7 +35,6 @@ class AND(PBF):
     def __str__(self):
         return str(self.lchild) + " " + str(self.rchild) + " &"
 
-
 class NOT(PBF):
 
     def __init__(self, f):
@@ -47,7 +42,6 @@ class NOT(PBF):
 
     def __str__(self):
         return str(self.child) + " !"
-
 
 class PROP(PBF):
 
@@ -58,8 +52,43 @@ class PROP(PBF):
         return self.prop
 
 
-#PBFObj = AND(PROP("x"), NOT(OR(PROP("y"), PROP("z"))))
+def parse(postfix_str):
+    PBFObj = PBF()
+    temp_stack = []
+    postfix_str = postfix_str.replace(' ', '')
+    for it in range(len(postfix_str)):
+        if (postfix_str[it].isalpha()):
+            temp_stack.append(postfix_str[it])
+
+        elif (postfix_str[it] == '!'):
+            t1 = temp_stack.pop()
+            PBFObj = NOT(PROP(t1))
+            t1 = t1 + " " + '!'
+            temp_stack.append(t1)
+
+        elif (postfix_str[it] == '&'):
+            t1 = temp_stack.pop()
+            t2 = temp_stack.pop()
+            PBFObj = AND(PROP(t2), PROP(t1))
+            t1 = t2 + " " + t1 + " " + '&'
+            temp_stack.append(t1)
+
+        else:
+            t1 = temp_stack.pop()
+            t2 = temp_stack.pop()
+            PBFObj = OR(PROP(t2), PROP(t1))
+            t1 = t2 + " " + t1 + " " + '|'
+            temp_stack.append(t1)
+
+    del temp_stack[:]
+    return PBFObj
+
+
+PBFObj = AND(PROP("x"), NOT(OR(PROP("y"), PROP("z"))))
 #PBFObj = AND(PROP("x"), AND(NOT(PROP("y")), NOT(PROP("z"))))
+#PBFObj = AND(PROP("w"), NOT(AND(NOT(OR(PROP("x"), PROP("y"))), PROP("z"))))
+#PBFObj = AND(PROP("w"), OR(PROP("x"), OR(PROP("y"), NOT(PROP("z")))))
+print "Defined PBF Object: "
 print PBFObj
 res = PBFObj.isNNF()
 if(res == True):
@@ -67,5 +96,12 @@ if(res == True):
 else:
     print "Given PBF is not in NNF"
 
-#nnf_str = PBFObj.toNNF()
-#print "NNF string = " + nnf_str
+#nnfObj = PBFObj.toNNF()
+#if(PBFObj.isNNF()):
+#    print "NNF string = " + PBFObj.__str__()
+#else:
+#    print nnfObj
+expr_str = "x y ! z ! | &"
+print "String to parse function = " + expr_str
+PBFObj = parse(expr_str)
+print "PBFObj from parse function: " + PBFObj.__str__()
