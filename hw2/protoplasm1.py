943fc1681d77e38203b53de7ef077faf3610c129
Vinay Krishnamurthy <vinayk.url@gmail.com>
2013-02-19 19:46:03 -0500

    Tokenization done using tokenize module

--- hw2/protoplasm1.py
+++ hw2/protoplasm1.py
@@ -1,57 +1,22 @@
-import ast
+import cStringIO, tokenize
 
-bin_op_map = {'Add': '+', 'Sub': '-', 'Mult': '*', 'Div': '/', 'Assign': '='}
-un_op_map = {'USub': '-'}
-precedence = {'+': 0, '-': 0, '*': 1, '/': 1}
+def get_tokens(lines):
+    token_list = []
+    token_list = tokenize.generate_tokens(cStringIO.StringIO(lines).readline)
 
-class customParser(ast.NodeVisitor):
-    
-    def __init__(self):
-        pass
+    return token_list
 
-    def generic_visit(self, node):
-        node_type = type(node).__name__
-
-        if(node_type in bin_op_map):
-            print bin_op_map[node_type]
-
-        elif (node_type in un_op_map):
-            print un_op_map[node_type]
-
-        else:
-            """ Do nothing """
-
-        ast.NodeVisitor.generic_visit(self, node)
-
-    def visit_Load(self, node):
-        pass
-
-    def visit_Name(self, node):
-        print node.id
-
-    def visit_Num(self, node):
-        print str(node.__dict__['n'])
-
-"""
-def well_formed(ast_obj, expr_ast):
-    def_var = []
-    use_var = []
-    ast_obj.visit(expr_ast, def_var, use_var)
-"""
-
-pobj = customParser()
-expr = str()
-io = str()
 with open('example1.proto') as f:
     lines = f.readlines()
+token_list = []
 for line in lines:
-    if(not "input" in line and not "print" in line):
-        expr += str(line)
+    if('input' in line or 'print' in line):
+        print line
     else:
-        io += str(line)
+        tokens = get_tokens(line)
+        for x in tokens:
+            if (x[1] == '\n' or x[1] == ' ' or x[1] == '' or x[1] == ';'):
+                continue
+            token_list.append(x[1])
 
-expr_ast = ast.parse(expr)
-print "Expressions: "
-pobj.visit(expr_ast)
-print "I/O statements: "
-print io
+print token_list
